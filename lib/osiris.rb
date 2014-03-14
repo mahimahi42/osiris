935b5b72c59b321de0066e1b4dba840c6a250604
Bryce Davis <BAD1128@gmail.com>
2014-03-14 13:38:57 -0400

    Added executable

--- lib/osiris.rb
+++ lib/osiris.rb
@@ -3,6 +3,13 @@ require "osiris/version"
 module Osiris
   extend self
   
+  # Package the website folder into a .tar.gz archive
+  #
+  # @param folder [String] the site's folder
+  # @return [String] the name of the created archive 
+  #
+  # @example
+  #   Osiris.package_website("site_dir") #=> "site_dir.tar.gz"
   def package_website(folder)
   	return "TODO"
   end
